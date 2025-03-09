1a920e2400bccc333e8b2117b7b341b1e41c0fc4
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-09 13:25:57 +0200

    Models.py for app users formatted

--- users/models.py
+++ users/models.py
@@ -6,12 +6,13 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name="Email")
 
-    phone_number = models.CharField(max_length=35, verbose_name="Телефон", blank=True, null=True,
-                                    help_text="Укажите номер телефона")
-    city = models.CharField(max_length=150, verbose_name="Город", blank=True, null=True,
-                                    help_text="Укажите город")
-    avatar = models.ImageField(upload_to="users/avatars/", verbose_name="Аватар", blank=True, null=True,
-                               help_text="Загрузите свой аватар")
+    phone_number = models.CharField(
+        max_length=35, verbose_name="Телефон", blank=True, null=True, help_text="Укажите номер телефона"
+    )
+    city = models.CharField(max_length=150, verbose_name="Город", blank=True, null=True, help_text="Укажите город")
+    avatar = models.ImageField(
+        upload_to="users/avatars/", verbose_name="Аватар", blank=True, null=True, help_text="Загрузите свой аватар"
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
