6c52d98502e3085643d3aa6fcaa34d5dc10d8e45
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-15 17:27:29 +0200

    Project urls fixed

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("lms/", include("lms.urls", namespace="lms"))
+    path("", include("lms.urls", namespace="lms"))
 ]
 
 if settings.DEBUG:
