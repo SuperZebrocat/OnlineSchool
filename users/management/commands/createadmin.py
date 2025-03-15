6796493896bb5bdc8829023d8c2a6434bc077036
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-15 18:06:06 +0200

    Custom command for admin creation realized

---
+++ users/management/commands/createadmin.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='adminuser@example.com',
+        )
+
+        user.set_password('1234qwer')
+
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save()
+
+        self.stdout.write(self.style.SUCCESS(f'Successfully created admin user with {user.email}!'))
