a07ffce070f366a1b1f7dc452bc90e5572b1c9bc
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-15 17:29:33 +0200

    Field lessons count in CourseSerializer added

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,15 +1,20 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from lms.models import Course, Lesson
 
 
-class CourseSerializer(ModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
 
+    def get_lessons_count(self, instance):
+        return instance.lessons.count()
 
-class LessonSerializer(ModelSerializer):
+
+class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
