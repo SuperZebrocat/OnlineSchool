c56c687be0a51c940ec5514c3e999603dac422f9
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-16 12:56:13 +0200

    Urls for users app registered in project urls

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("lms.urls", namespace="lms"))
+    path("", include("lms.urls", namespace="lms")),
+    path("", include("users.urls", namespace="users"))
 ]
 
 if settings.DEBUG:
