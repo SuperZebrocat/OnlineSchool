b2df40f95f0637a7c7517f7eec59feb15e09efb4
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-17 18:49:33 +0200

    Urls for course detail view created

--- lms/urls.py
+++ lms/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import SimpleRouter
 
 from lms.apps import LmsConfig
 from lms.views import (CourseViewSet, LessonCreateAPIView, LessonDestroyAPIView, LessonListAPIView,
-                       LessonRetrieveAPIView, LessonUpdateAPIView)
+                       LessonRetrieveAPIView, LessonUpdateAPIView, CourseDetailAPIView)
 
 app_name = LmsConfig.name
 
@@ -15,7 +15,9 @@ urlpatterns = [
     path("lessons/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
     path("lessons/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson_update"),
     path("lessons/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_detail"),
-    path("lessons/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson_delete")
+    path("lessons/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson_delete"),
+    path("courses/<int:pk>/", CourseDetailAPIView.as_view(), name="course_lessons_list")
+
 ]
 
 urlpatterns += router.urls
