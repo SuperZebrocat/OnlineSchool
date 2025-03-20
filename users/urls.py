5dd621e78a9db046b9db640fa387b3cdade941ee
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-20 16:39:54 +0200

    Urls for token views added in users app

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from users.apps import UsersConfig
 from users.views import PaymentCreateAPIView, PaymentListAPIView, PaymentRetrieveAPIView, PaymentUpdateAPIView, \
     PaymentDestroyAPIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = UsersConfig.name
 
@@ -11,4 +15,6 @@ urlpatterns = [
     path("payments/<int:pk>", PaymentRetrieveAPIView.as_view(), name="payment_detail"),
     path("payments/<int:pk>/update/", PaymentUpdateAPIView.as_view(), name="payment_detail"),
     path("payments/<int:pk>/delete/", PaymentDestroyAPIView.as_view(), name="payment_delete"),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
