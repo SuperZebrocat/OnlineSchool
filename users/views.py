834e9a4bc888e60b40e60d9fadce5ee497d0b39a
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-20 18:49:42 +0200

    Permission for user creation added for any

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
 from users.models import Payment, User
 from users.serializers import PaymentSerializer, UserSerializer
 from django_filters.rest_framework import DjangoFilterBackend
@@ -34,6 +36,7 @@ class PaymentDestroyAPIView(generics.DestroyAPIView):
 class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
