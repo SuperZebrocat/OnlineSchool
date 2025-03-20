338d19e240478174cd4da12142e2946c520c2bd6
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-20 17:39:12 +0200

    Url for user creation view registered

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from users.apps import UsersConfig
 from users.views import PaymentCreateAPIView, PaymentListAPIView, PaymentRetrieveAPIView, PaymentUpdateAPIView, \
-    PaymentDestroyAPIView
+    PaymentDestroyAPIView, UserCreateAPIView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -15,6 +15,7 @@ urlpatterns = [
     path("payments/<int:pk>", PaymentRetrieveAPIView.as_view(), name="payment_detail"),
     path("payments/<int:pk>/update/", PaymentUpdateAPIView.as_view(), name="payment_detail"),
     path("payments/<int:pk>/delete/", PaymentDestroyAPIView.as_view(), name="payment_delete"),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
