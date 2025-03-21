5c57aea634bed77ff007b97daddf264ca4c57cff
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-21 22:39:09 +0200

    User model registered in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from users.models import Payment
+from users.models import Payment, User
 
 admin.site.register(Payment)
 
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ('id', 'email')
