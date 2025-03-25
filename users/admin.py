539f928f90e29868b892a2a77c1229cf31f7a439
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-25 10:10:50 +0200

    Custom creation form added in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from users.forms import UserCreationForm
 from users.models import Payment, User
 
 admin.site.register(Payment)
@@ -6,4 +8,12 @@ admin.site.register(Payment)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    add_form = UserCreationForm
     list_filter = ('id', 'email')
+
+    def save_model(self, request, obj, form, change):
+        if not change:  # Если это создание нового пользователя
+            password = form.cleaned_data.get('password')
+            if password:
+                obj.set_password(password)  # Хэшируем пароль
+        super().save_model(request, obj, form, change)
