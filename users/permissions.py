6c7d605754674d9f65836920b2bf809635ab8777
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-25 11:07:31 +0200

    Permissions for users groups realized

--- users/permissions.py
+++ users/permissions.py
@@ -2,6 +2,21 @@ from rest_framework import permissions
 
 
 class IsModer(permissions.BasePermission):
-
+    """Проверяет, является ли пользователь модератором."""
     def has_permission(self, request, view):
         return request.user.groups.filter(name="moders").exists()
+
+
+class IsNotModer(permissions.BasePermission):
+    """Проверяет, что пользователь является не модератором."""
+
+    def has_permission(self, request, view):
+        return not request.user.groups.filter(name="moders").exists()
+
+
+class IsOwner(permissions.BasePermission):
+    """Проверяет, является ли пользователь владельцем."""
+    def has_object_permission(self, request, view, obj):
+        if obj.owner == request.user:
+            return True
+        return False
