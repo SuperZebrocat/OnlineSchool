f84c782ef5b922793dc511b1e29d9692eca6f953
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-28 21:09:00 +0200

    APIView for subscription model registered in app urls

--- lms/urls.py
+++ lms/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import SimpleRouter
 
 from lms.apps import LmsConfig
 from lms.views import (CourseViewSet, LessonCreateAPIView, LessonDestroyAPIView, LessonListAPIView,
-                       LessonRetrieveAPIView, LessonUpdateAPIView)
+                       LessonRetrieveAPIView, LessonUpdateAPIView, SubscriptionAPIView)
 
 app_name = LmsConfig.name
 
@@ -16,6 +16,7 @@ urlpatterns = [
     path("lessons/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson_update"),
     path("lessons/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_detail"),
     path("lessons/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson_delete"),
+    path("subscription/", SubscriptionAPIView.as_view(), name="subscription")
 ]
 
 urlpatterns += router.urls
