bab5356687543fcd3bede9297751c4c9933d7f87
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-28 21:08:22 +0200

    APIView for subscription model created

--- lms/views.py
+++ lms/views.py
@@ -1,9 +1,11 @@
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import generics
-from lms.models import Course, Lesson
-from lms.serializers import CourseSerializer, LessonSerializer
+from lms.models import Course, Lesson, Subscription
+from lms.serializers import CourseSerializer, LessonSerializer, SubscriptionSerializer
 from users.permissions import IsModer, IsOwner, IsNotModer
 
 
@@ -65,3 +67,29 @@ class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = (IsAuthenticated, IsOwner)
+
+
+class SubscriptionAPIView(APIView):
+    """Представление для установки и удаления подписки на обновления курса."""
+    serializer_class = SubscriptionSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        course_id = request.data.get('course_id')
+        course_item = get_object_or_404(Course, id=course_id)
+
+        # Проверяем, существует ли подписка
+        subs_item = Subscription.objects.filter(user=user, course=course_item)
+
+        if subs_item.exists():
+            # Удаляем подписку
+            subs_item.delete()
+            message = 'Подписка удалена'
+        else:
+            # Создаем подписку
+            Subscription.objects.create(user=user, course=course_item)
+            message = 'Подписка добавлена'
+
+        # Возвращаем ответ в API
+        return Response({"message": message})
