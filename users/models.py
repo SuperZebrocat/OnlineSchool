da121fca70660885d9975239060da5189ee44d07
екатерина ермакова <katka_z_jones@mail.ru>
2025-04-19 17:32:37 +0200

    Payment model updated for stripe functionality

--- users/models.py
+++ users/models.py
@@ -39,12 +39,20 @@ class Payment(models.Model):
     payment_date = models.DateTimeField(auto_now_add=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True, null=True, related_name="payment")
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, blank=True, null=True, related_name="payment")
-    amount = models.DecimalField(max_digits=20, decimal_places=2, verbose_name="Сумма оплаты")
-    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHODS, verbose_name="Способ оплаты")
+    amount = models.PositiveIntegerField(verbose_name="Сумма платежа", help_text="Укажите сумму платежа")
+    payment_method = models.CharField(
+        max_length=10, choices=PAYMENT_METHODS, verbose_name="Способ оплаты", help_text="Укажите способ оплаты"
+    )
+    session_id = models.CharField(
+        max_length=255, blank=True, null=True, verbose_name="Id сессии", help_text="Укажите Id сессии"
+    )
+    link = models.URLField(
+        max_length=400, blank=True, null=True, verbose_name="Ссылка на оплату", help_text="Укажите ссылку на оплату"
+    )
 
     class Meta:
-        verbose_name = 'Платеж'
-        verbose_name_plural = 'Платежи'
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
 
     def __str__(self):
-        return f'{self.payment_date} - {self.amount}'
+        return f"{self.payment_date} - {self.amount}"
