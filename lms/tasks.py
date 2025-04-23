7978516b120fd7f972b03decd5249a1e48289c9e
екатерина ермакова <katka_z_jones@mail.ru>
2025-04-23 23:55:16 +0200

    Periodic task for user deactivation created

--- lms/tasks.py
+++ lms/tasks.py
@@ -1,5 +1,10 @@
+from django.utils import timezone
+from datetime import timedelta
+
 from celery import shared_task
 from django.core.mail import send_mail
+
+from users.models import User
 
 
 @shared_task
@@ -10,3 +15,16 @@ def send_update_mail(course_name, subscriber_email):
         from_email=None,
         recipient_list=[subscriber_email],
     )
+
+
+@shared_task
+def check_user_login():
+    users = User.objects.all()
+
+    for user in users:
+        if user.last_login is not None:
+            month_after_last_login = user.last_login + timedelta(30)
+            if month_after_last_login < timezone.now():
+                user.is_active = False
+                user.save()
+                print(f"Пользователь {user.email} заблокирован")
