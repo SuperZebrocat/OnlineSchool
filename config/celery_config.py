7e6a46215d399633e64e2ca07052d20db8d61228
екатерина ермакова <katka_z_jones@mail.ru>
2025-04-23 19:53:07 +0200

    Celery configured

--- config/celery_config.py
+++ config/celery_config.py
@@ -1,15 +1,15 @@
 from __future__ import absolute_import, unicode_literals
+
+import eventlet
+eventlet.monkey_patch()  # noqa: E402
+
 import os
 from celery import Celery
 
-# Установка переменной окружения для настроек проекта
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-# Создание экземпляра объекта Celery
-app = Celery('config')
+app = Celery('celery')
 
-# Загрузка настроек из файла Django
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Автоматическое обнаружение и регистрация задач из файлов tasks.py в приложениях Django
 app.autodiscover_tasks()
