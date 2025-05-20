b437da16f69f309667d9a5f26fb0e071864d60c6
екатерина ермакова <katka_z_jones@mail.ru>
2025-05-20 23:38:20 +0200

    Project fixed

--- config/settings.py
+++ config/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("NAME"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("HOST"),
         "PORT": os.getenv("PORT"),
     }
